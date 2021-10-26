92553f247bd84ad0eb4d4a139070515a956cfa85
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2021-10-26 21:33:42 +0700

    edit 2 files

--- pedulilindungi2/urls.py
+++ pedulilindungi2/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+import biodata.urls as biodata
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('biodata/', include('biodata.urls')),
 ]
