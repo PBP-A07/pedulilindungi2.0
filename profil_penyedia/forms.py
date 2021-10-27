f4c9aad274459165b820c10e6adde22ce860bbf3
rakha.rayhan <rakha.rayhan@ui.ac.id>
2021-10-27 07:54:39 +0700

    working on some Profil Penyedia templates and styling

---
+++ profil_penyedia/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import widgets
+#from biodata.models import Penyedia
+
+# class EditPenyediaForm(forms.ModelForm):
+#     class Meta:
+#         model = Penyedia
+#         fields = ['nama', 'kota', 'nomor_telepon', 'alamat']
--- profil_penyedia/views.py
+++ profil_penyedia/views.py
@@ -1,3 +1,17 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+#from biodata.models import Penyedia
+#from .forms import EditPenyediaForm
 
 # Create your views here.
+def profil_penyedia(request):
+    #penyedia = Penyedia.objects.all()
+    return render(request, 'profil_penyedia.html')
+
+# def ubah_data_penyedia(request):
+#     penyedia = Penyedia.objects.all()
+#     form = EditPenyediaForm(request.POST or None, instance=penyedia)
+#     if (form.is_valid and request.method == 'POST'):
+#         form.save()
+#         return HttpResponseRedirect('/profil-penyedia')
+#     return render(request, 'ubah_data_penyedia.html')
