f6ad89ebdf10c69ada9684e04ac4f25e435886f5
Alifiyah Nur Rochmah Ariandri <alifiyahariandri@gmail.com>
2021-10-27 15:17:56 +0700

    update landing page style

--- main/models.py
+++ main/models.py
@@ -1,7 +1,6 @@
 
 from django.db import models
 from django.contrib.auth.models import User
-from account.models import Profile
 
 class Questions(models.Model) : 
     id = models.AutoField(primary_key=True)
