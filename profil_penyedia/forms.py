19cc28111bf0f5c3e9fa5c54daf37ccfc51ac5dc
rakha.rayhan <rakha.rayhan@ui.ac.id>
2021-10-27 15:14:10 +0700

    saving some progress in profil_penyedia

--- profil_penyedia/forms.py
+++ profil_penyedia/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from django.forms import widgets
-#from biodata.models import Penyedia
+from biodata.models import Penyedia
 
-# class EditPenyediaForm(forms.ModelForm):
-#     class Meta:
-#         model = Penyedia
-#         fields = ['nama', 'kota', 'nomor_telepon', 'alamat']
+class EditPenyediaForm(forms.ModelForm):
+    class Meta:
+        model = Penyedia
+        fields = ['namaInstansi', 'kota', 'nomorTelepon', 'alamat']
+        widgets = {
+            'namaInstansi': forms.TextInput(attrs={'class': 'form_input', 'placeholder': "Masukkan nama instansi kamu"}),
+            'kota': forms.TextInput(attrs={'class': 'form_input', 'placeholder': "Masukkan kota instansi kamu"}),
+            'nomorTelepon': forms.TextInput(attrs={'class': 'form_input', 'placeholder': "Masukkan nomor telepon instansi kamu"}),
+            'alamat': forms.TextInput(attrs={'class': 'form_input', 'placeholder': "Masukkan alamat instansi kamu"})
+        }
--- profil_penyedia/views.py
+++ profil_penyedia/views.py
@@ -1,17 +1,36 @@
 from django.http.response import HttpResponseRedirect
+from django.contrib.auth.models import User
 from django.shortcuts import render
-#from biodata.models import Penyedia
-#from .forms import EditPenyediaForm
+from biodata.models import Penyedia
+from .forms import EditPenyediaForm
 
 # Create your views here.
-def profil_penyedia(request):
-    #penyedia = Penyedia.objects.all()
-    return render(request, 'profil_penyedia.html')
+# versi 1
+# def profil_penyedia(request):
+#     response = {'nama': request.user.profile.namaInstansi,
+#                 'kota': request.user.profile.kota,
+#                 'nomor_telepon': request.user.profile.nomorTelepon,
+#                 'alamat': request.user.profile.alamat}
+#     return render(request, 'profil_penyedia.html', response)
 
-# def ubah_data_penyedia(request):
-#     penyedia = Penyedia.objects.all()
-#     form = EditPenyediaForm(request.POST or None, instance=penyedia)
-#     if (form.is_valid and request.method == 'POST'):
-#         form.save()
-#         return HttpResponseRedirect('/profil-penyedia')
-#     return render(request, 'ubah_data_penyedia.html')
+#versi 2
+# def profil_penyedia(request):
+#     penyedia = User.objects.get(username=request.user.username)
+#     response = {'nama': penyedia.profile.namaInstansi,
+#                 'kota': penyedia.profile.kota,
+#                 'nomor_telepon': penyedia.profile.nomorTelepon,
+#                 'alamat': penyedia.profile.alamat}
+#     return render(request, 'profil_penyedia.html', response)
+
+#versi 3
+def profil_penyedia(request):
+    response = {'x': User.objects.get(username=request.user.username)}
+    return render(request, 'profil_penyedia.html', response)
+
+def ubah_data_penyedia(request):
+    penyedia = Penyedia.objects.all()
+    form = EditPenyediaForm(request.POST or None, instance=penyedia)
+    if (form.is_valid and request.method == 'POST'):
+        form.save()
+        return HttpResponseRedirect('/profil-penyedia')
+    return render(request, 'ubah_data_penyedia.html')
