2405ccfd7e06ce54a6a56f1a77eea3c5d9a4553c
adnptrpw <andini.putri02@ui.ac.id>
2021-10-30 02:19:14 +0700

    Revert "cleaning up"
    This reverts commit aa435b1855504b55f1618e1ec247b2cf178434ea.

--- daftar_vaksin/forms.py
+++ daftar_vaksin/forms.py
@@ -1,5 +1,7 @@
 from django import forms
+from django.db.models import fields
 from django.forms import ModelForm
+from tambah_vaksin.models import Vaksin
 from .models import JadwalVaksin
 from biodata.models import Penyedia
 
