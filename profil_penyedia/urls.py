40677e047497fbba4df603f90dab34dda0d8322c
rakha.rayhan <rakha.rayhan@ui.ac.id>
2021-11-01 06:40:34 +0700

    changed pesan image, added conditionals in views, not yet implemented lihat peserta feature

--- profil_penyedia/urls.py
+++ profil_penyedia/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import profil_penyedia, ubah_data_penyedia, lihat_pendaftar, catatan_penyedia, tambah_catatan
+from .views import *
 
 urlpatterns = [
-    path('', profil_penyedia),
-    path('ubah-data-penyedia', ubah_data_penyedia),
-    path('lihat-pendaftar', lihat_pendaftar),
-    path('catatan-penyedia', catatan_penyedia),
-    path('tambah-catatan', tambah_catatan),
+    path('', index, name='index'),
+    path('user/<usn>', profil_penyedia, name='profil_penyedia'),
+    path('ubah-data-penyedia', ubah_data_penyedia, name='ubah_data_penyedia'),
+    path('lihat-pendaftar', lihat_pendaftar, name='lihat_pendaftar'),
+    path('catatan-penyedia', catatan_penyedia, name='catatan_penyedia'),
+    path('tambah-catatan', tambah_catatan, name='tambah_catatan'),
 ]
--- profil_penyedia/views.py
+++ profil_penyedia/views.py
@@ -1,4 +1,4 @@
-from django.http.response import HttpResponseRedirect, HttpResponseForbidden
+from django.http.response import Http404, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from biodata.models import Penyedia
@@ -8,7 +8,20 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 @login_required(login_url='/auth/login/')
-def profil_penyedia(request):
+def index(request):
+    return HttpResponseRedirect('/profil-penyedia/user/' + request.user.username)
+
+@login_required(login_url='/auth/login/')
+def profil_penyedia(request, usn):
+    if request.user.username != usn:
+        return HttpResponseForbidden("The Username does not match with yours")
+
+    elif request.user.profile.role == 'penerima':
+        return HttpResponseRedirect('/profil-penerima')
+
+    elif request.user.profile.role != 'penyedia':
+        return HttpResponseForbidden('Currently you have no role. Please contact the admin to assign you a role.')
+
     penyedia = Penyedia.objects.get(superUser = request.user)
     response = {'nama': penyedia.namaInstansi,
                 'kota': penyedia.kota,
@@ -18,6 +31,9 @@ def profil_penyedia(request):
 
 @login_required(login_url='/auth/login/')
 def ubah_data_penyedia(request):
+    if request.user.profile.role != 'penyedia':
+        return HttpResponseForbidden("You are not allowed to access this page because of your current role")
+
     penyedia = Penyedia.objects.get(superUser = request.user)
     form = EditPenyediaForm(request.POST or None, instance = penyedia)
     if (form.is_valid() and request.method == 'POST'):
@@ -26,17 +42,19 @@ def ubah_data_penyedia(request):
     return render(request, 'ubah_data_penyedia.html', {'form': form})
 
 @login_required(login_url='/auth/login/')
-def lihat_pendaftar(request):
-    return render(request, 'lihat_pendaftar.html')
-
-@login_required(login_url='/auth/login/')
 def catatan_penyedia(request):
-    notes = CatatanPenyedia.objects.filter(owner = request.user)
+    if request.user.profile.role != 'penyedia':
+        return HttpResponseForbidden("You are not allowed to access this page because of your current role")
+
+    notes = reversed(CatatanPenyedia.objects.filter(owner = request.user))
     response = {'notes': notes}
     return render(request, 'catatan_penyedia.html', response)
 
 @login_required(login_url='/auth/login/')
 def tambah_catatan(request):
+    if request.user.profile.role != 'penyedia':
+        return HttpResponseForbidden("You are not allowed to access this page because of your current role")
+
     form = CatatanPenyediaForm(request.POST or None)
     if (form.is_valid() and request.method == 'POST'):
         f = form.save(commit=False)
@@ -44,3 +62,7 @@ def tambah_catatan(request):
         f.save()
         return HttpResponseRedirect('/profil-penyedia/catatan-penyedia')
     return render(request, 'form_catatan_penyedia.html')
+
+@login_required(login_url='/auth/login/')
+def lihat_pendaftar(request):
+    return render(request, 'lihat_pendaftar.html')
