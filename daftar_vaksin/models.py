807d82c70b1a5716a706454592673ba6473e59e8
adnptrpw <andini.putri02@ui.ac.id>
2021-11-03 20:53:11 +0700

    change max_length

--- daftar_vaksin/models.py
+++ daftar_vaksin/models.py
@@ -2,10 +2,10 @@ from django.db import models
 from biodata.models import Penyedia, Peserta
 
 class JadwalVaksin(models.Model):
-   kota = models.CharField(max_length=30)
+   kota = models.CharField(max_length=255)
    tanggal = models.DateField(blank=False, null=True)
-   jenis_vaksin = models.CharField(max_length=30, default='')
+   jenis_vaksin = models.CharField(max_length=255, default='')
    place = models.ForeignKey(Penyedia, on_delete=models.CASCADE, null=True)
-   tempat = models.CharField(max_length=30)
+   tempat = models.CharField(max_length=255)
    penerima = models.ForeignKey(Peserta, on_delete=models.CASCADE, null=True)
 
