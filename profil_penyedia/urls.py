d5639d4b0d36c4a4e903dbcf190a4459793ed2dc
rakha.rayhan <rakha.rayhan@ui.ac.id>
2021-11-03 14:49:18 +0700

    fixed minor typing issue

--- profil_penyedia/urls.py
+++ profil_penyedia/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('user/<usn>', profil_penyedia, name='profil_penyedia'),
     path('ubah-data-penyedia', ubah_data_penyedia, name='ubah_data_penyedia'),
-    path('lihat-pendaftar', lihat_pendaftar, name='lihat_pendaftar'),
+    # path('lihat-pendaftar', lihat_pendaftar, name='lihat_pendaftar'),
     path('catatan-penyedia', catatan_penyedia, name='catatan_penyedia'),
     path('tambah-catatan', tambah_catatan, name='tambah_catatan'),
 ]
