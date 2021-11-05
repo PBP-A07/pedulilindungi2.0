eddda9647c68574ef592c9782c66e5e0dc60faae
Rayyan Azka <rayyan.azka@ui.ac.id>
2021-11-05 17:25:06 +0700

    Fix models.py

--- tambah_vaksin/models.py
+++ tambah_vaksin/models.py
@@ -4,11 +4,6 @@ from biodata.models import Penyedia
 # Create your models here.
 class Vaksin(models.Model):
     jenis = models.CharField(max_length=30)
-<<<<<<< HEAD
     tanggal = models.DateField()
-=======
-    tanggal = models.DateField(blank=False, null=True)
-    waktu = models.TimeField()
->>>>>>> fdebf8e278c4ffb3409c93fe2520ec961cbfd821
     jumlah = models.SmallIntegerField()
     penyedia = models.ForeignKey(Penyedia, on_delete=models.CASCADE, null=True, blank=True)
