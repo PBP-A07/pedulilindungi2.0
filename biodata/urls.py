28f099c464bc413948ee457a0affcb4b31792b19
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2021-12-21 10:53:16 +0700

    edit sintax for urls.py

--- biodata/urls.py
+++ biodata/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import *
+from .views import biodata_peserta, biodata_penyedia, ajax_posting_peserta, ajax_posting_penyedia, peserta_flutter, penyedia_flutter
 
 urlpatterns = [
-    path('peserta_form', biodata_peserta, name='peserta'), 
-    path('penyedia_form', biodata_penyedia, name='penyedia'),
-    path('peserta/create', ajax_posting_peserta),
-    path('penyedia/create', ajax_posting_penyedia),
-    path('peserta/flutter', peserta_flutter),
-    path('penyedia/flutter', penyedia_flutter),
+    path('peserta_form', biodata_peserta, name='peserta_form'), 
+    path('penyedia_form', biodata_penyedia, name='penyedia_form'),
+    path('peserta/create', ajax_posting_peserta, name='ajax_posting_peserta'),
+    path('penyedia/create', ajax_posting_penyedia, name='ajax_posting_penyedia'),
+    path('peserta/flutter', peserta_flutter, name='peserta_flutter'),
+    path('penyedia/flutter', penyedia_flutter, name='penyedia_flutter'),
 ]
