7f703c6ccb90261e2e0daddc4e3f845f48cfea10
rakha.rayhan <rakha.rayhan@ui.ac.id>
2021-12-28 20:13:12 +0700

    save current progress of profil penyedia's urls and views

--- profil_penyedia/views.py
+++ profil_penyedia/views.py
@@ -1,4 +1,4 @@
-from django.http.response import Http404, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseRedirect
+from django.http.response import Http404, HttpResponseBadRequest, HttpResponseForbidden, HttpResponseNotAllowed, HttpResponseRedirect, HttpResponse, JsonResponse
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from biodata.models import Penyedia, Peserta
@@ -6,6 +6,9 @@ from daftar_vaksin.models import JadwalVaksin
 from .models import CatatanPenyedia
 from .forms import CatatanPenyediaForm, EditPenyediaForm
 from django.contrib.auth.decorators import login_required
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 # Create your views here.
 @login_required(login_url='/auth/login/')
@@ -90,3 +93,49 @@ def lihat_pendaftar(request):
     list_orang.reverse()
     response = {'people': list_orang}
     return render(request, 'lihat_pendaftar.html', response)
+
+def json_catatan_penyedia(request):
+    data_catatan_penyedia = serializers.serialize("json", reversed(CatatanPenyedia.objects.filter(owner = request.user)))
+    return HttpResponse(data_catatan_penyedia, content_type="application/json")
+
+def json_profil_penyedia(request):
+    data_profil_penyedia = serializers.serialize("json", Penyedia.objects.filter(superUser = request.user))
+    return HttpResponse(data_profil_penyedia, content_type="application/json")
+
+def json_lihat_pendaftar(request):
+    penyedia = Penyedia.objects.get(superUser = request.user)
+    jenis = JadwalVaksin.objects.filter(place = penyedia).values('jenis_vaksin')
+    tanggal = JadwalVaksin.objects.filter(place = penyedia).values('tanggal')
+    penerima = JadwalVaksin.objects.filter(place = penyedia).values('penerima')
+    list_orang = []
+    dict_orang = dict()
+    for i in range(penerima.count()):
+        person = Peserta.objects.get(id = penerima[i].get('penerima'))
+        dict_orang['nama']=person.namaLengkap
+        dict_orang['nik']=person.NIK
+        dict_orang['nomor_handphone']=person.nomorHandphone
+        dict_orang['jenis_vaksin']=jenis[i].get('jenis_vaksin')
+        dict_orang['tanggal']=tanggal[i].get('tanggal')
+        list_orang.append(dict_orang.copy())
+    list_orang.reverse()
+    data_lihat_pendaftar = serializers.serialize("json", list_orang)
+    return HttpResponse(data_lihat_pendaftar, content_type="application/json")
+
+@csrf_exempt
+def flutter_catatan_penyedia(request):
+    if request.method == 'POST':
+        value = json.loads(request.body.decode('utf-8'))
+        title_val = value['title']
+        message_val = value['message']
+
+        if title_val and message_val:
+            CatatanPenyedia.objects.create(
+                title = title_val,
+                message = message_val,
+                # owner = User.objects.get(username = request.user.username)
+            )
+
+            response = {
+                'notification': "Catatan Anda berhasil ditambahkan"
+            }
+        return JsonResponse(response)
